4910e9f3243d3e8ac6213ae79638e323bfe994a1
AbiCarsonBrown <142737359+AbiCarsonBrown@users.noreply.github.com>
2023-11-18 16:19:55 +0000

    Create GET coffeshop by ID endpoint

--- routes/coffeeshops.js
+++ routes/coffeeshops.js
@@ -12,4 +12,32 @@ router.route("/").get(async (_req, res) => {
   }
 });
 
+router.route("/:id").get(async (req, res) => {
+  try {
+    const coffeeShop = await knex("coffeeshop")
+      .where({ "coffeeshop.id": req.params.id })
+      .select(
+        "coffeeshop.id",
+        "coffeeshop.coffeeshop_name",
+        "coffeeshop.address",
+        "coffeeshop.description"
+      );
+
+    const visits = await knex("visit")
+      .where({ "visit.coffeeshop_id": req.params.id })
+      .join("user", "visit.user_id", "user.id")
+      .select(
+        "user.username",
+        "visit.visited",
+        "visit.on_wishlist",
+        "visit.rating",
+        "visit.review"
+      );
+
+    res.status(200).json([coffeeShop, visits]);
+  } catch (error) {
+    res.status(400).send(`Error retrieving coffee shop: ${error}`);
+  }
+});
+
 module.exports = router;
