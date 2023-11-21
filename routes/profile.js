b42cb7fd382b7ce8e1c0b10a14cd226f069b2b84
AbiCarsonBrown <142737359+AbiCarsonBrown@users.noreply.github.com>
2023-11-21 13:55:44 +0000

    Create get request for user's wishlist

--- routes/profile.js
+++ routes/profile.js
@@ -7,11 +7,41 @@ router.route("/been").get(authenticate, async (req, res) => {
   try {
     const data = await knex("visit")
       .where({ "visit.user_id": req.user_id })
-      .where({ visited: true });
+      .where({ visited: true })
+      .join("coffeeshop", "visit.coffeeshop_id", "coffeeshop.id")
+      .select(
+        "visit.id",
+        "visit.coffeeshop_id",
+        "coffeeshop.coffeeshop_name",
+        "coffeeshop.address",
+        "visit.on_wishlist",
+        "visit.rating",
+        "visit.review"
+      );
 
     res.status(200).json(data);
   } catch (error) {
     res.status(400).send(`Error retrieving visited coffee shops: ${error}`);
+  }
+});
+
+router.route("/next").get(authenticate, async (req, res) => {
+  try {
+    const data = await knex("visit")
+      .where({ "visit.user_id": req.user_id })
+      .where({ on_wishlist: true })
+      .join("coffeeshop", "visit.coffeeshop_id", "coffeeshop.id")
+      .select(
+        "visit.id",
+        "visit.coffeeshop_id",
+        "coffeeshop.coffeeshop_name",
+        "coffeeshop.address",
+        "visit.visited"
+      );
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).send(`Error retrieving coffee shop wishlist: ${error}`);
   }
 });
 
