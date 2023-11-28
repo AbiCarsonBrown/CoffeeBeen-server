0303b7fe2bceaa076b384d74e40fc92e47175b3d
AbiCarsonBrown <142737359+AbiCarsonBrown@users.noreply.github.com>
2023-11-28 13:21:01 +0000

    Alter .env for deployment

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 module.exports = {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
